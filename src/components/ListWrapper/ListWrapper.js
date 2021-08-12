b2c9d1bd293d73b20620c672229ccdd505ae0e05
Kai Mareving <karska.joanna@gmail.com>
2021-08-12 23:19:49 +0200

    4.5

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
 import './ListWrapper.css';
-import { twitterAccounts } from '../../data/twitterAccounts';
 
- const ListWrapper = () => (
+const ListWrapper = (props) => (
   <ul className='ListWrapper__wrapper'>
-     {twitterAccounts.map((item, index) => (
-       <ListItem key={index} {...item} />
-      //or <ListItem key={item.name} {...item} />
-     ))}
+    {props.items.map((item, index) => (
+      <ListItem key={index} {...item} />
+    //or <ListItem key={item.name} {...item} />
+    ))}
   </ul>
 );
 
