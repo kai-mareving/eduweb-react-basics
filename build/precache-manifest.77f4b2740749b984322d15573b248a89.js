90ebd27340849b909ee840ebc66852c06d65b051
Kai Mareving <karska.joanna@gmail.com>
2021-08-25 15:54:14 +0200

    6.1 Deploy

---
+++ build/precache-manifest.77f4b2740749b984322d15573b248a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c631de8926718c8407d",
+    "url": "/static/css/main.b79bd05f.chunk.css"
+  },
+  {
+    "revision": "6c631de8926718c8407d",
+    "url": "/static/js/main.6c631de8.chunk.js"
+  },
+  {
+    "revision": "a93801f7837be254fa9e",
+    "url": "/static/js/1.a93801f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "0f304236255b38654676a64ec0af19c1",
+    "url": "/index.html"
+  }
+];
